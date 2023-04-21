258fa74df23f857e3f1da3a38c80adf09e84ea7c
yogita03 <patilyogita408@gmail.com>
2023-04-22 00:10:02 +0530

    Restaurant Adding Form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 
@@ -11,7 +12,7 @@ const MealItem = (props) => {
             <div className={classes.price}>{price}</div>
         </div>
         <div>
-          
+          <MealItemForm />
         </div>
     </li>
    );
